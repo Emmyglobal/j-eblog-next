3fd79f8ff390dd5779f10abc8f16700cbc5bd1db
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-11-10 12:36:01 +0100

    push

---
+++ components/CommentSection.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function CommentSection(): JSX.Element {
+  return (
+    <div id="comments-section" className="mt-10 border-t pt-6">
+      <h3 className="text-xl font-semibold mb-4">💬 Comments</h3>
+      <p className="text-gray-600">Comments feature coming soon...</p>
+    </div>
+  );
+}
