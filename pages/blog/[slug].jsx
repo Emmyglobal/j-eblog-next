38bd30ac1854d24316b4d96b172eb9aeb9bc5037
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-11-10 13:16:39 +0100

    Enter

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -4,7 +4,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown";
-//import CommentSection from "../../components/CommentSection";
+import CommentSection from "../../components/CommentSection";
 
 export async function getStaticPaths() {
   const files = fs.readdirSync(path.join("content"));
